620da0da2139a6a542cd59d48934de900f89c3da
Tekin Bicer <bicer@anl.gov>
2015-06-29 14:56:46 -0500

    Cleaned up the struct in main file.

--- src/sirt/sirt_main.cc
+++ src/sirt/sirt_main.cc
@@ -23,27 +23,6 @@ std::string const kReconDatasetPath="/data";
 
 int const iteration=10;
 
-struct {
-  std::string const kProjectionFilePath="/Users/bicer/Projects/data/original/13-ID/13id1_fixed_1s.h5";
-  //std::string const kProjectionFilePath="/Users/bicer/Projects/data/original/13-ID/13id1_fixed_16s.h5";
-  //std::string const kProjectionFilePath="/Users/bicer/Projects/tomopy/shepp-tekin.h5";
-  //std::string const kProjectionFilePath="/Users/bicer/Projects/data/original/13-ID/13id_x8_16s.h5";
-  //std::string const kProjectionFilePath="/Users/bicer/Projects/data/original/13-ID/13id_x8.h5";
-  std::string const kProjectionDatasetPath="/exchange/data";
-  std::string const kThetaFilePath="/Users/bicer/Projects/data/original/13-ID/13id1_fixed_1s.h5";
-  //std::string const kThetaFilePath="/Users/bicer/Projects/data/original/13-ID/13id1_fixed_16s.h5";
-  //std::string const kThetaFilePath="/Users/bicer/Projects/tomopy/shepp-tekin.h5";
-  //std::string const kThetaFilePath="/Users/bicer/Projects/data/original/13-ID/13id_x8_16s.h5";
-  //std::string const kThetaFilePath="/Users/bicer/Projects/data/original/13-ID/13id_x8.h5";
-  std::string const kThetaDatasetPath="/exchange/theta";
-  std::string const kReconOutputPath="./13id_i1.h5";
-  std::string const kReconDatasetPath="/data";
-
-  int const iteration=2;
-  int const thread_count=0;
-
-} TraceRuntimeConfig;
-
 int main(int argc, char **argv)
 {
   /* Initiate middleware's communication layer */
