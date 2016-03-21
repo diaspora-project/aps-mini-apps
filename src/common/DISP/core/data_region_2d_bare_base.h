b8a835efb7e8921d86ddd8597ef13045035a068f
Tekin Bicer <bicer@anl.gov>
2016-03-21 13:23:09 -0500

    DISP is moved to the common folder

---
+++ src/common/DISP/core/data_region_2d_bare_base.h
@@ -0,0 +1,121 @@
+/** \file data_region_2d_bare_base.h 
+ */
+#ifndef DISP_SRC_DISP_DATA_REGION_2D_BARE_BASE_H
+#define DISP_SRC_DISP_DATA_REGION_2D_BARE_BASE_H
+
+/**
+ * \class DataRegion2DBareBase
+ * \brief This class uses DataRegionBareBase in order to provide 
+ * replicated output objects.
+ *
+ * Contact: bicer@anl.gov
+ */
+
+#include <iostream>
+#include <vector>
+#include "data_region_bare_base.h"
+
+template <typename T> 
+class DataRegion2DBareBase {
+  private:
+    std::vector<DataRegionBareBase<T>*> regions_;
+
+    size_t rows_;
+    size_t cols_;
+
+  public:
+    DataRegion2DBareBase(size_t rows, size_t cols){
+      regions_.reserve(rows);
+
+      for(size_t i=0; i<rows; i++)
+        regions_.push_back(new DataRegionBareBase<T>(cols));
+
+      rows_ = rows;
+      cols_ = cols;
+    }
+
+    ~DataRegion2DBareBase(){
+      for(auto &region : regions_)
+        delete region;
+      rows_ = 0;
+      cols_ = 0;
+    }
+
+    /* Copy constructor */
+    DataRegion2DBareBase(const DataRegion2DBareBase &dr){
+      regions_.reserve(dr.rows());
+
+      for(auto t : dr.regions()){
+        auto r = new DataRegionBareBase<T>(*t);
+        regions_.push_back(r);
+      }
+
+      rows_ = dr.rows();
+      cols_ = dr.cols();
+    }
+
+    /* Copy assignment */
+    DataRegion2DBareBase<T>& operator=(const DataRegion2DBareBase &dr){
+      if(this==&dr) return *this;
+
+      for(auto rg : regions_)
+        delete rg;
+
+      regions_.clear();
+      regions_.reserve(dr.rows());
+
+      for(auto rg : dr.regions())
+        regions_.push_back(new DataRegionBareBase<T>(*rg));
+      rows_ = dr.rows();
+      cols_ = dr.cols();
+
+      return *this;
+    }
+
+    /* TODO: Move constructor & assignment */
+
+    DataRegionBareBase<T>& operator[](size_t row) const { 
+      return *(regions_[row]); 
+    }
+
+    const std::vector<DataRegionBareBase<T>*>& regions() const { return regions_; };
+    size_t num_rows() const { return rows_; }
+    size_t num_cols() const { return cols_; }
+    size_t rows() const { return rows_; }
+    size_t cols() const { return cols_; }
+    size_t count() const { return rows_*cols_; }
+    size_t size() const {return rows_*cols_*sizeof(T); }
+
+
+    T& item(size_t row, size_t col) const {
+      if(row >= rows_ || col >= cols_)
+        throw std::out_of_range("Tried to access out of range offset!");
+      return (*regions_[row])[col];
+    }
+
+    void item(size_t row, size_t col, T &val) const {
+      if(row >= rows_ || col >= cols_)
+        throw std::out_of_range("Tried to access out of range offset!");
+      (*regions_[row])[col] = val;
+    }
+
+    void ResetAllItems(T &val) const {
+      for(size_t i=0; i<rows_; i++)
+        for(size_t j=0; j<cols_; j++)
+          (*regions_[i])[j] = val;
+    }
+
+    void ResetAllMirroredRegions(){
+      for(size_t i=0; i<rows_; i++)
+        (*regions_[i]).ResetMirroredRegionIter();
+    }
+
+    void copy(DataRegion2DBareBase<T> &dr){
+      if(dr.rows() != rows_ || dr.cols() != cols_)
+        throw std::out_of_range("DataRegions' ranges do not overlap!");
+
+      dr = *this;
+    }
+};
+
+#endif    // DISP_SRC_DISP_DATA_REGION_2D_BARE_BASE_H
---
+++ src/common/DISP/core/reduction_space_a.h
@@ -0,0 +1,73 @@
+#ifndef _REDUCTION_SPACE_BASE_A_
+#define _REDUCTION_SPACE_BASE_A_
+
+#include "data_region_2d_bare_base.h"
+#include "mirrored_region_bare_base.h"
+
+/// CT: Derived class type
+/// DT: Data type on which Reduce function operate
+template <typename CT, typename DT>
+class AReductionSpaceBase{
+  private:
+    DataRegion2DBareBase<DT> *reduction_objects_ = nullptr;
+
+  public:
+    void Process(MirroredRegionBareBase<DT> &input) {
+      static_cast<CT*>(this)->Reduce(input);
+    };
+
+    // Default operation is sum
+    virtual void LocalSynchWith(CT &input_reduction_space) {
+      auto &ri = input_reduction_space.reduction_objects();
+      auto &ro = *reduction_objects_;
+
+      if(ri.num_rows()!=ro.num_rows() || ri.num_cols()!=ro.num_cols())
+        throw std::range_error("Local and destination reduction objects have different dimension sizes!");
+
+      for(size_t i=0; i<ro.num_rows(); i++)
+        for(size_t j=0; j<ro.num_cols(); j++)
+          ro[i][j] += ri[i][j];
+    };
+
+
+    // Derived class can use this function to perform
+    // deep copies
+    virtual void CopyTo(CT &target)=0;
+
+    virtual CT *Clone(){
+      if(reduction_objects_ == nullptr)
+        throw std::invalid_argument("reduction objects point to nullptr!");
+
+      auto &red_objs = *reduction_objects_;
+
+      CT *cloned_obj = new CT(red_objs.rows(), red_objs.cols());
+      (*cloned_obj).reduction_objects() = red_objs;
+
+      static_cast<CT*>(this)->CopyTo(*cloned_obj);
+
+      return cloned_obj;
+    };
+
+    DataRegion2DBareBase<DT>& reduction_objects() {
+      return *reduction_objects_; 
+    };
+
+    DataRegionBareBase<DT>& operator[](int row){ 
+      return (*reduction_objects_)[row]; 
+    };
+
+    AReductionSpaceBase(DataRegion2DBareBase<DT> *reduction_objects) :
+      reduction_objects_(reduction_objects) {
+    };
+
+    AReductionSpaceBase(size_t rows, size_t cols){
+      reduction_objects_ = new DataRegion2DBareBase<DT>(rows, cols);
+    };
+
+    /// Make sure to call derived class (CT) destructor 
+    /// instead of base class (this one) destructor 
+    virtual ~AReductionSpaceBase(){ 
+      delete reduction_objects_; 
+    };
+};
+#endif
