6feda6e3391067dc322a3e3e8cfbbbff6259f85d
Tekin Bicer <bicer@anl.gov>
2023-07-31 09:27:14 -0500

    uploading the sample command files

---
+++ tests/daq.cmd
@@ -0,0 +1,1 @@
+python DAQStream.py --mode 1 --simulation_file /dev/shm/tomo_00058_all_subsampled1p.h5 --d_iteration 1 --publisher_addr tcp://*:50000 --iteration_sleep 1 --synch_addr tcp://*:50001 --synch_count 1
---
+++ tests/sirt.cmd
@@ -0,0 +1,1 @@
+./sirt_stream --dest-host 127.0.0.1 --dest-port 50010 --window-iter 1 --window-step 4 --window-length 4 -t 2 -c 1427 --pub-addr tcp://*:52000
