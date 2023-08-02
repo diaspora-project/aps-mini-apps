195caad9969156acb3ef9408127cc168253c6c76
Tekin Bicer <tbicer@anl.gov>
2023-08-02 11:43:34 -0500

    single command to create container and execute commands for the stream reconstruction

--- docker/sirt/sirt.docker.sh
+++ docker/sirt/sirt.docker.sh
@@ -1,3 +1,2 @@
 docker build --no-cache -f Dockerfile.StreamerSIRT -t streamer-sirt:latest --target StreamerSIRT .
-docker run -d --rm --name streamer-sirt -p 52000:52000 streamer-sirt:latest 
-docker exec /Trace/build/bin/sirt_stream --write-freq 4 --dest-host host.docker.internal --dest-port 50010 --window-iter 1 --window-step 4 --window-length 4 -t 2 -c 1427 --pub-addr tcp://*:52000
+docker run --rm -it --name streamer-sirt -p 52000:52000 streamer-sirt:latest /Trace/build/bin/sirt_stream --write-freq 4 --dest-host host.docker.internal --dest-port 50010 --window-iter 1 --window-step 4 --window-length 4 -t 2 -c 1427 --pub-addr tcp://*:52000
