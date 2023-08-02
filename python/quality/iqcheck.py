dbb4d649f63f0aa2e9df36dcb731278c4bb75545
Tekin Bicer <tbicer@anl.gov>
2023-08-02 13:00:04 -0500

    quality check app is updated for the docker

--- python/quality/iqcheck.py
+++ python/quality/iqcheck.py
@@ -2,41 +2,40 @@ from sewar.full_ref import msssim, mse, ssim, uqi, psnr
 from skimage.exposure import adjust_gamma, rescale_intensity
 import h5py as h5
 import numpy as np
+import sys
 
 def rescale(reconstruction, hi):
   I = rescale_intensity(reconstruction, out_range=(0., 1.))
   return adjust_gamma(I, 1, hi)
 
-fid1, fid2 = 0, 203
-hi = 1
-f1 = f"/home/beams2/TBICER/projects/Trace/build/bin/{fid1:06d}-recon.h5"
-f2 = f"/home/beams2/TBICER/projects/Trace/build/bin/{fid2:06d}-recon.h5"
+if __name__ == "__main__":
+  if len(sys.argv) != 3:
+    print("Usage: python iqcheck.py <fid1> <fid2>")
+    sys.exit(1)
+  
+  f1 = sys.argv[1]
+  f2 = sys.argv[2]
 
-# read hdf5 file and return numpy array
-with h5.File(f1, 'r') as f:
-  recon0 = np.array(f['data'])
+  hi = 1
 
-with h5.File(f2, 'r') as f:
-  recon1 = np.array(f['data'])
+  # read hdf5 file and return numpy array
+  with h5.File(f1, 'r') as f:
+    recon0 = np.array(f['data'])
 
-recon0 = rescale(np.rot90(recon0[0])[::-1], hi)
-recon1 = rescale(np.rot90(recon1[0])[::-1], hi)
+  with h5.File(f2, 'r') as f:
+    recon1 = np.array(f['data'])
 
-# readjust the range of the image to 0-255
-recon0i = (recon0 * 255).astype(np.uint8)
-recon1i = (recon1 * 255).astype(np.uint8)
+  recon0 = rescale(np.rot90(recon0[0])[::-1], hi)
+  recon1 = rescale(np.rot90(recon1[0])[::-1], hi)
 
-q = msssim(recon1i, recon0i)
-print("MS-SSIM: {}".format(q))
+  # readjust the range of the image to 0-255
+  recon0i = (recon0 * 255).astype(np.uint8)
+  recon1i = (recon1 * 255).astype(np.uint8)
 
-q = ssim(recon1i, recon0i)
-print("SSIM: {}".format(q))
+  q0 = msssim(recon1i, recon0i)
+  q1 = ssim(recon1i, recon0i)
+  q2 = uqi(recon1, recon0)
+  q3 = mse(recon1, recon0)
+  q4 = psnr(recon1i, recon0i)
 
-q = uqi(recon1, recon0)
-print("UQI: {}".format(q))
-
-q = mse(recon1, recon0)
-print("MSE: {}".format(q))
-
-q = psnr(recon1i, recon0i)
-print("PSNR: {}".format(q))
+  print("MS-SSIM: {}; SSIM: {}; UQI: {}; MSE: {}; PSNR: {}".format(q0, q1, q2, q3, q4))
