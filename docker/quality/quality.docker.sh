478e4827786a729372cb6eee5218db0bff546404
Tekin Bicer <tbicer@anl.gov>
2023-08-04 11:35:05 -0500

    More info on the usage of the apps with docker.

--- docker/quality/quality.docker.sh
+++ docker/quality/quality.docker.sh
@@ -1,2 +1,2 @@
 docker build --no-cache -f Dockerfile.StreamerQuality -t streamer-quality:latest .
-docker run --rm -v /Users/tbicer/projects/data/output:/mnt/output -it --name streamer-quality streamer-quality:latest python /Trace/build/python/quality/iqcheck.py ${INPUT0} ${INPUT1}
+docker run --rm -v ~/projects/data/output:/mnt/output -it --name streamer-quality streamer-quality:latest python /Trace/build/python/quality/iqcheck.py ${INPUT0} ${INPUT1}
--- docker/sirt/sirt.docker.sh
+++ docker/sirt/sirt.docker.sh
@@ -1,2 +1,2 @@
 docker build --no-cache -f Dockerfile.StreamerSIRT -t streamer-sirt:latest --target StreamerSIRT .
-docker run --rm -v /Users/tbicer/projects/data/output:/mnt/output -it --name streamer-sirt -p 52000:52000 streamer-sirt:latest /Trace/build/bin/sirt_stream --write-freq 4 --dest-host host.docker.internal --dest-port 50010 --window-iter 1 --window-step 4 --window-length 4 -t 2 -c 1427 --pub-addr tcp://*:52000 --recon-output-dir /mnt/output
+docker run --rm -v ~/projects/data/output:/mnt/output -it --name streamer-sirt -p 52000:52000 streamer-sirt:latest /Trace/build/bin/sirt_stream --write-freq 4 --dest-host host.docker.internal --dest-port 50010 --window-iter 1 --window-step 4 --window-length 4 -t 2 -c 1427 --pub-addr tcp://*:52000 --recon-output-dir /mnt/output
