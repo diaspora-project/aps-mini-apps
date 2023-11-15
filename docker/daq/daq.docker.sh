ec4b3cd6d59f3e081634aa86f870daaa2cf9b153
Tekin <tekinbicer@users.noreply.github.com>
2023-11-15 13:31:50 -0600

    container caching is enabled

--- docker/daq/daq.docker.sh
+++ docker/daq/daq.docker.sh
@@ -1,2 +1,2 @@
-docker build --no-cache -f Dockerfile.StreamerDAQ -t streamer-daq:latest --target StreamerDAQ .
+docker build -f Dockerfile.StreamerDAQ -t streamer-daq:latest --target StreamerDAQ .
 docker run --rm -it --name streamer-daq -p 50000-50001:50000-50001 streamer-daq:latest python /Trace/build/python/streamer-daq/DAQStream.py --mode 1 --simulation_file /Trace/data/tomo_00058_all_subsampled1p_s1079s1081.h5 --d_iteration 1 --publisher_addr tcp://*:50000 --iteration_sleep 1 --synch_addr tcp://*:50001 --synch_count 1
