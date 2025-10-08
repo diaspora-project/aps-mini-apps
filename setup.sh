de21a21cc22b12d121fe0962f93318f0c5fd03c1
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2025-10-08 16:48:33 +0000

    reconfigure Parsl for Polaris execution

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ cmake -S .. -B build \
   -DCMAKE_C_COMPILER=cc \
   -DCMAKE_CXX_COMPILER=CC \
   -DCMAKE_Fortran_COMPILER=ftn \
-  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_CXX_FLAGS_RELEASE \
   -DCMAKE_PREFIX_PATH=/home/ndhai/diaspora/src/spack/var/spack/environments/APS/.spack-env/view
 cmake --build build -j
 mkdir -p python/streamer-sirt
