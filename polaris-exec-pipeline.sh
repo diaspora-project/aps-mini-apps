1ede7abf0e12367a0ab985150352ac1f43af6b6c
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2025-10-08 15:43:06 +0000

    reconfigure Parsl for Polaris execution

---
+++ polaris-exec-pipeline.sh
@@ -0,0 +1,75 @@
+# Force kill all processes if press CTRL+C or if any command fails
+trap "pkill -9 -f 'bedrock'; pkill -9 -f 'streamer-daq'; pkill -9 -f 'streamer-dist'; pkill -9 -f 'sirt_stream'; pkill -9 -f 'streamer-den'; exit 1" SIGINT SIGTERM EXIT
+
+set -e
+
+echo "Cleaning up previous runs --------------------------------------------"
+pkill -9 -f "bedrock" || true
+pkill -9 -f "streamer-daq" || true
+pkill -9 -f "streamer-dist" || true
+pkill -9 -f "streamer-sirt" || true
+pkill -9 -f "sirt_stream" || true
+pkill -9 -f "streamer-den" || true
+pkill -INT -f "veloc" || true
+pkill -9 -f "veloc-backend" || true
+pkill -9 -f "FailureInjector" || true
+
+# Remove previous checkpoints
+rm -rf /tmp/scratch/*
+rm -rf /tmp/persistent/*
+
+# Check if the number of arguments is corre
+if [ "$#" -ne 4 ]; then
+    echo "Usage: exec-pipeline.sh <sirt_ranks> <num_sinograms>"
+    echo "  <sirt_ranks>    Number of SIRT workers/processes"
+    echo "  <sirt_tasks>    Number of SIRT tasks/threads"
+    echo "  <num_sinograms> Number of sinograms to process"
+    echo "  <mtbf>         Mean time between failures (in seconds)"
+    exit 1
+fi
+sirt_ranks=$1
+sirt_tasks=$2
+num_sinograms=$3
+mtbf=$4
+
+DATE=$(date +"%Y-%m-%d-%Hh%Mmin%Ssec")
+logdir=build/logs/D${DATE}
+mkdir -p ${logdir}
+echo "Logging execution information at ${logdir}"
+ln -sfn "`pwd`/${logdir}" "build/logs/latest"
+echo "Updated symlink: ${latest_link} -> ${logdir}"
+
+
+echo "Start Mofka server ---------------------------------------------------"
+bash run-mofka.sh > ${logdir}/mofka.out 2> ${logdir}/mofka.err &
+echo bash run-mofka.sh
+sleep 10
+
+echo "Start DAQ ------------------------------------------------------------"
+bash run-daq.sh ${sirt_ranks} ${sirt_tasks} ${num_sinograms} ${logdir} > ${logdir}/daq.out 2> ${logdir}/daq.err &
+echo bash run-daq.sh ${sirt_ranks} ${sirt_tasks} ${num_sinograms} ${logdir}
+sleep 10
+
+echo "Start DIST -----------------------------------------------------------"
+bash run-dist.sh ${num_sinograms} ${sirt_tasks} ${logdir} > ${logdir}/dist.out 2> ${logdir}/dist.err &
+echo bash run-dist.sh ${num_sinograms} ${sirt_tasks} ${logdir}
+# sleep 10
+
+echo "Start SIRT -----------------------------------------------------------"
+bash run-sirt.sh ${sirt_ranks} ${logdir} > ${logdir}/sirt.out 2> ${logdir}/sirt.err &
+echo bash run-sirt.sh ${sirt_ranks} ${logdir}
+
+# echo "Start Exp Control ----------------------------------------------------"
+bash run-exp-control.sh ${mtbf} ${logdir} 2> ${logdir}/exp-control.err | tee ${logdir}/exp-control.out &
+echo bash run-exp-control.sh ${mtbf} ${logdir}
+
+echo "Start DEN ------------------------------------------------------------"
+echo bash run-den.sh ${sirt_tasks} ${logdir}
+bash run-den.sh ${sirt_tasks} ${logdir} 2> ${logdir}/den.err | tee ${logdir}/den.out
+
+
+echo "Clean up after run ---------------------------------------------------"
+pkill -9 -f "FailureInjector" || true
+echo "COMPLETE"
+
+
---
+++ run-sirt-polaris.sh
@@ -0,0 +1,67 @@
+source activate-spack.sh
+# source envpy/bin/activate
+
+# Check if the number of parameters is correct
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters"
+    echo "Usage: run-sirt.sh <number of processes> <logdir>"
+    exit 1
+fi
+
+sirt_ranks=$1
+logdir=$2
+
+trap "echo 'Ctrl+C pressed. Terminating...'; exit 1" SIGINT SIGTERM
+
+# mpiexec -n $sirt_ranks ./build/bin/sirt_stream \
+#     --write-freq 4  \
+#     --window-iter 1 \
+#     --window-step 4 \
+#     --window-length 4 \
+#     -t 4 \
+#     -c 1427 \
+#     --protocol na+sm \
+#     --group-file mofka.json \
+#     --batchsize 4
+
+# # # PURE C++ EXECUTION. No parallelism, for testing only
+# build/bin/sirt_stream \
+#     --id 0 \
+#     --np ${sirt_ranks} \
+#     --write-freq 4  \
+#     --window-iter 1 \
+#     --window-step 4 \
+#     --window-length 4 \
+#     --thread 4 \
+#     --center 1427 \
+#     --protocol na+sm \
+#     --group-file mofka.json \
+#     --batchsize 4 \
+#     --reconOutputPath ./output.h5 \
+#     --recon-output-dir . \
+#     --reconDatasetPath /data \
+#     --pub-freq 10000 \
+#     --ckpt-freq 4 \
+#     --ckpt-name sirt \
+#     --ckpt-config veloc.cfg \
+#     --logdir ${logdir}
+
+# gdb arguments
+# run --id 0 --np 1 --write-freq 4 --window-iter 1 --window-step 4 --window-length 4 --thread 1 --center 1427 --protocol na+sm --group-file mofka.json --batchsize 4 --reconOutputPath ./output.h5 --recon-output-dir . --reconDatasetPath /data --pub-freq 10000 --ckpt-freq 4 --ckpt-name sirt --ckpt-config veloc.cfg --logdir .
+
+python -u ./build/python/streamer-sirt/ParslSirtPolaris.py \
+    --num-workers ${sirt_ranks} \
+    --write-freq 4  \
+    --window-iter 1 \
+    --window-step 4 \
+    --window-length 4 \
+    --thread 4 \
+    --center 1427 \
+    --protocol na+sm \
+    --group-file mofka.json \
+    --batchsize 4 \
+    --ckpt-freq 4 \
+    --ckpt-name sirt \
+    --ckpt-config veloc.cfg \
+    --recon-output-dir ./build/denoise \
+    --logdir ${logdir}
