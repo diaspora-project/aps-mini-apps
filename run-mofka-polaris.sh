e533599f851ccb2b8b76d36c17eca604f49c2f6d
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2025-10-14 19:54:49 +0000

    polaris multiple process exp

---
+++ run-mofka-polaris.sh
@@ -0,0 +1,9 @@
+source activate-spack.sh
+# source envpy/bin/activate
+
+# bedrock na+sm -c config.json
+bedrock cxi -v trace -c config.json
+
+# Run the following if needed
+# /usr/sbin/sysctl kernel.yama.ptrace_scope=0
+
