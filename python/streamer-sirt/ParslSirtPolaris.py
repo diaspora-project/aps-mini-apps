29694278c17b1099cb25e87debfd7643e4ae2720
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2025-11-10 22:46:38 -0600

    enable Parsl run on multiple node with LocalProvider and MPIExecLauncher

--- python/streamer-sirt/ParslSirtPolaris.py
+++ python/streamer-sirt/ParslSirtPolaris.py
@@ -48,7 +48,7 @@ user_opts = {
     # "worker_init":      f"source /path/to/your/virtualenv/bin/activate; cd {run_dir}", # load the environment where parsl is installed
     "worker_init":      f"cd {run_dir}", # load the environment where parsl is installed
     "scheduler_options":"#PBS -l filesystems=home:eagle" , # specify any PBS options here, like filesystems
-    "account":          "Diaspora",
+    "account":          "diaspora",
     "queue":            "debug-scaling",
     "walltime":         "1:00:00",
     "nodes_per_block":  3, # think of a block as one job on polaris, so to run on the main queues, set this >= 10
