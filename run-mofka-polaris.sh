37d09699a6f7173325cdb85e0b1fed04a19eff66
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2025-11-10 23:08:05 -0600

    enable Parsl run on multiple node with LocalProvider and MPIExecLauncher

--- run-mofka-polaris.sh
+++ run-mofka-polaris.sh
@@ -1,8 +1,8 @@
 source activate-spack.sh
 # source envpy/bin/activate
 
-bedrock na+sm -c config.json
-# bedrock cxi -v trace -c config.json
+# bedrock na+sm -c config.json
+bedrock cxi -v trace -c config.json
 
 # Run the following if needed
 # /usr/sbin/sysctl kernel.yama.ptrace_scope=0
