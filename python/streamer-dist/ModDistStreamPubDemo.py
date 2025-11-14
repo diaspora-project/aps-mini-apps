b7adcf256a2d5731bbbbe0deb18b1106dfc57b64
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2025-11-14 03:30:35 -0600

    change number of sirt_dist_action to number of sirt tasks

--- python/streamer-dist/ModDistStreamPubDemo.py
+++ python/streamer-dist/ModDistStreamPubDemo.py
@@ -107,6 +107,18 @@ def main():
       print(f"Send info to sirt: {assign_info}")
       action_producer.flush()
 
+  for w in range(num_workers):
+    print(f"Worker {w} assigned tasks: {task_to_worker[w]}")
+    for t in range(len(task_to_worker[w])):
+      assign_info = {
+          "Type": "START_TASK",
+          "worker_id": w,
+          "task_id": task_to_worker[w][t]
+      }
+      action_producer.push(assign_info)
+      print(f"Send info to sirt: {assign_info}")
+      action_producer.flush()
+
 
   mofka_producing_time = []
   mofka_consuming_time = []
