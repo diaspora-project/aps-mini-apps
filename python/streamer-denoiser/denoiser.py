ff1262d05404f7e04d7e954055726d17b20ca8ff
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2025-12-23 15:09:29 -0600

    allow enable/disable sst

--- python/streamer-denoiser/denoiser.py
+++ python/streamer-denoiser/denoiser.py
@@ -120,7 +120,7 @@ def main(input_path, recon_path, model_path, protocol, group_file, batchsize, nu
             break
         else:
 
-            print("Received data for iteration stream ", m["iteration_stream"], " rank ", m["rank"])
+            # print("Received data for iteration stream ", m["iteration_stream"], " rank ", m["rank"])
 
             iteration_stream = m["iteration_stream"]
             row_id = int(m["rank"])
