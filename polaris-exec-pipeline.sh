cc6cd29d580b9fea34f4661c64e9404601e198bd
Hai Duc Nguyen <hai.nguyen@anl.gov>
2026-01-22 07:03:25 +0000

    prepare polaris setup

--- polaris-exec-pipeline.sh
+++ polaris-exec-pipeline.sh
@@ -71,7 +71,7 @@ node_dist=${nodes_array[0]}
 # node_sirts=${nodes_array[0]}
 node_sirts=$nodes_array
 node_den=${nodes_array[0]}
-node_mofka=${nodes_array[0],nodes_array[1]}
+node_mofka="${nodes_array[0]} ${nodes_array[1]}"
 # node_mofka=${nodes_array[0]}
 node_control=${nodes_array[0]}
 
@@ -89,11 +89,11 @@ start_ns=$(date +%s%N)
 start_iso=$(date -Iseconds)
 
 echo "Start Mofka server ---------------------------------------------------"
-mpiexec --no-vni -ppn 1 -d 16 --hosts $node_mofka bash $exec_dir/run-mofka-polaris.sh > "${logdir}/mofka.out" 2> "${logdir}/mofka.err" &
+mpiexec --no-vni -ppn 1 -d 16 --hosts "$(IFS=,; echo "${node_mofka[*]}")" bash $exec_dir/run-mofka-polaris.sh > "${logdir}/mofka.out" 2> "${logdir}/mofka.err" &
 # mpiexec -ppn 1 -d 16 --hosts $node_mofka bedrock cxi -v trace -c config.json > "${logdir}/mofka.out" 2> "${logdir}/mofka.err" &
 # mpiexec --no-vni -n 1 -ppn 1 -d 16 --hosts $node_mofka bedrock na+sm -c config.json > "${logdir}/mofka.out" 2> "${logdir}/mofka.err" &
 # bedrock na+sm -c config.json > "${logdir}/mofka.out" 2> "${logdir}/mofka.err" &
-echo mpiexec --no-vni -ppn 1 -d 16 --hosts $node_mofka bash $exec_dir/run-mofka-polaris.sh
+echo mpiexec --no-vni -ppn 1 -d 16 --hosts "$(IFS=,; echo "${node_mofka[*]}")" bash $exec_dir/run-mofka-polaris.sh
 sleep 10
 
 echo "Start DAQ ------------------------------------------------------------"
