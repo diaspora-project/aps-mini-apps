cd7db20d5c3566b1142557d1e46c8135e3fbe186
Hai Duc Nguyen <hai.nguyen@anl.gov>
2026-01-23 04:34:44 +0000

    prepare polaris setup

--- run-mofka-polaris.sh
+++ run-mofka-polaris.sh
@@ -1,15 +1,16 @@
 source activate-spack.sh
 # source envpy/bin/activate
 
-if [[ "$PMI_RANK" -eq 0 ]]; then
-    echo rank=$PMI_RANK: Running bedrock...
-    bedrock cxi -v trace -c config.json
-else
-    echo rank=$PMI_RANK: Sleeping...
-    sleep 10000
-fi
+# if [[ "$PMI_RANK" -eq 0 ]]; then
+#     echo rank=$PMI_RANK: Running bedrock...
+#     bedrock cxi -v trace -c config.json
+# else
+#     echo rank=$PMI_RANK: Sleeping...
+#     sleep 10000
+# fi
+
 # bedrock na+sm -c config.json
-# bedrock cxi -v trace -c config.json
+bedrock cxi -v trace -c config.json
 
 # Run the following if needed
 # /usr/sbin/sysctl kernel.yama.ptrace_scope=0
