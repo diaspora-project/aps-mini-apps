f13a0e302fdafde7f4beb9cff8a51e9735a0281f
Hai Duc Nguyen <hai.nguyen@anl.gov>
2026-01-23 05:25:03 +0000

    prepare polaris setup

--- python/streamer-denoiser/denoiser.py
+++ python/streamer-denoiser/denoiser.py
@@ -114,6 +114,10 @@ def main(input_path, recon_path, model_path, protocol, group_file, batchsize, nu
             for iteration_stream in waiting_metadata.keys():
                 print(f"    --> Waiting iteration {iteration_stream}: Complete tasks: {waiting_metadata[iteration_stream].keys()}")
 
+        else:
+            print("Complete as all tasks already sent FIN")
+            break
+
         ts = time.perf_counter()
         f = consumer.pull()
         event = f.wait()
