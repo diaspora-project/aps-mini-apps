25c0989450435aa119aeee9297d75372028ae248
Hai Duc Nguyen <hai.nguyen@anl.gov>
2026-01-25 09:00:13 +0000

    update load balancing to reduce config parameters

--- python/streamer-dist/DynamicModDistStreamPubDemo.py
+++ python/streamer-dist/DynamicModDistStreamPubDemo.py
@@ -779,7 +779,6 @@ def task_to_worker_assignment(args, num_workers):
     try:
       f = action_consumer.pull()
       event = f.wait()
-      event.acknowledge()
       metadata = json.loads(event.metadata)
       if metadata["Type"] == "PROGRESS":
         task_id = metadata["task_id"]
