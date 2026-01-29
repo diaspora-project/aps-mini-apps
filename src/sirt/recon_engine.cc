10f9dd2cecc1ddc57a3622d28c1b785acc0514e0
Hai Duc Nguyen <hai.nguyen@anl.gov>
2026-01-29 01:44:58 +0000

    add ckpt overhead monitor

--- src/sirt/recon_engine.cc
+++ src/sirt/recon_engine.cc
@@ -476,7 +476,7 @@ int ReconTask::run() {
   }
   #endif
 
-  std::cout << "[Task-" << task_id << " Checkpoint overhead=" << ckpt_time << std::endl;
+  std::cout << "[Task-" << task_id << " Checkpoint overhead=" << ckpt_tot << std::endl;
 
   /* Clean-up the resources */
   std::cout << "[Task-" << task_id << "] Releasing local resources" << std::endl;
