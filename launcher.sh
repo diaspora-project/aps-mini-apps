83ca0af8627115b3043d4c5589aab21e54cb29cc
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2026-02-06 09:43:01 -0600

    update running script

--- launcher.sh
+++ launcher.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+PROJ=$PWD
 DATE=$(date +"%Y-%m-%d-%Hh%Mmin%Ssec")
 TOP=/lus/eagle/projects/Diaspora/ndhai/aps-mini-apps/failure-injection/periodic/D${DATE}/
 mkdir -p $TOP
+rsync -av --exclude='build' --exclude='data' --exclude='tests' --exclude='runinfo' $PROJ $TOP
 echo $TOP > recent-run
 cd $TOP
 qsub -o $TOP $HOME/diaspora/src/aps-mini-apps/polaris-test-scale.sh
