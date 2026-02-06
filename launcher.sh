690c65b5869930502abc3e1dbf156bab4058e41e
Hai Duc Nguyen <nguyenduchai.mt@gmail.com>
2026-02-06 13:24:14 -0600

    update launch

--- launcher.sh
+++ launcher.sh
@@ -3,7 +3,7 @@ PROJ=$PWD
 DATE=$(date +"%Y-%m-%d-%Hh%Mmin%Ssec")
 TOP=/lus/eagle/projects/Diaspora/ndhai/aps-mini-apps/scaling/D${DATE}/
 mkdir -p $TOP
-rsync -av --exclude='.git' --exclude='build' --exclude='data' --exclude='tests' --exclude='runinfo' $PROJ $TOP
+rsync -av --exclude='.git' --exclude='build' --exclude='tests' --exclude='runinfo' $PROJ $TOP
 echo $TOP > recent-run
 cd $TOP/aps-mini-apps
 qsub -o $TOP/aps-mini-apps polaris-test-scale.sh
