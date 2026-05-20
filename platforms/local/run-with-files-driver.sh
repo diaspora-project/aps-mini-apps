6bacc9c8bd513a109c37400d6d90163dc75f688e
Matthieu Dorier <mdorier@anl.gov>
2026-05-20 14:17:37 +0100

    Revert "removed unnecessary topics"
    This reverts commit a6c04348077b3b3e2588b792acd93335a7c97a05.

--- platforms/local/run-with-files-driver.sh
+++ platforms/local/run-with-files-driver.sh
@@ -18,8 +18,10 @@ echo "Starting topic creations"
 # setup topics and partitions
 # DAQ -> DIST topic
 diaspora-ctl topic create --name daq_dist $DIASPORA_CTL_DRIVER_ARGS --topic.num_partitions 1
-# DIST -> SIRT topic (one partition per SIRT rank)
+# DIST topics
 diaspora-ctl topic create --name dist_sirt $DIASPORA_CTL_DRIVER_ARGS --topic.num_partitions $SIRT_RANKS
+diaspora-ctl topic create --name handshake_s_d $DIASPORA_CTL_DRIVER_ARGS --topic.num_partitions 1
+diaspora-ctl topic create --name handshake_d_s $DIASPORA_CTL_DRIVER_ARGS --topic.num_partitions $SIRT_RANKS
 # SIRT -> DEN topic (one partition per SIRT rank to avoid concurrent write conflicts)
 diaspora-ctl topic create --name sirt_den $DIASPORA_CTL_DRIVER_ARGS --topic.num_partitions $SIRT_RANKS
 
